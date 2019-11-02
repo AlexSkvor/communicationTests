e2cb835e3538a575c7087d9032716bc823a00a3a
AlexSkvor <megoskvor95@yandex.ru>
2019-11-02 13:00:18 +0200

    layout for user choosing created

--- android/app/src/main/java/ru/lingstra/communications/data/database/entities/UserEntity.kt
+++ android/app/src/main/java/ru/lingstra/communications/data/database/entities/UserEntity.kt
@@ -6,6 +6,5 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "users")
 data class UserEntity(
     @PrimaryKey val id: String,
-    val name: String,
-    val password: String
+    val name: String
 )
---
+++ android/app/src/main/java/ru/lingstra/communications/ui/user/UserChoosingFragment.kt
@@ -0,0 +1,4 @@
+package ru.lingstra.communications.ui.user
+
+class UserChoosingFragment {
+}
